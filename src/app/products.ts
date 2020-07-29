a16212ce8d0820b3a24000f888efe948237a90de
Arkkarachat.S <arkkarachat.s@github.com>
2020-07-29 16:54:57 +0700

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,18 @@
+export const products =[
+  {
+    name: 'Phone XL',
+    price: 799,
+    description:'A large phone with one of the best screen'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description:'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 599,
+    description:'A best phone standard in the world'
+  }
+
+]
